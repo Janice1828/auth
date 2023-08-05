154fe8b8939cf42f50b5c8233607723561c1e1e6
Jenish Limbu <jlimbu539@gmail.com>
2023-08-05 12:23:00 +0545

    changes

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,46 @@
 import React from "react";
-
+import { FormControl } from "@material-ui/core";
+import InputLabel from "@material-ui/core/InputLabel/InputLabel";
+import Input from "@material-ui/core/Input/Input";
+import Button from "@material-ui/core/Button/Button";
+import Box from "@material-ui/core/Box/Box";
+import Container from "@material-ui/core/Container/Container";
+import { Link } from "react-router-dom";
+import Grid from "@material-ui/core/Grid/Grid";
+import Typography from "@material-ui/core/Typography/Typography";
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <Container>
+      <form>
+        <Grid container spacing={3}>
+          <Grid item xs={12}>
+            <Typography color="primary" variant="h4">
+              Login Form
+            </Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <FormControl fullWidth>
+              <InputLabel htmlFor="my-input">Email address</InputLabel>
+              <Input id="my-input" />
+            </FormControl>
+          </Grid>
+          <Grid item xs={12}>
+            <FormControl fullWidth>
+              <InputLabel htmlFor="my-input">Password</InputLabel>
+              <Input id="my-input" type="password" />
+            </FormControl>
+          </Grid>
+          <Grid item style={{ display: "flex", gap: "20px" }}>
+            <Button color="primary" variant="contained">
+              Login
+            </Button>
+            <div>
+              OR <Link to="/registration">Register?</Link>
+            </div>
+          </Grid>
+        </Grid>
+      </form>
+    </Container>
+  );
 };
-
 export default Login;
--- src/pages/Registration.tsx
+++ src/pages/Registration.tsx
@@ -2,16 +2,45 @@ import React from "react";
 import { FormControl } from "@material-ui/core";
 import InputLabel from "@material-ui/core/InputLabel/InputLabel";
 import Input from "@material-ui/core/Input/Input";
-import FormHelperText from "@material-ui/core/FormHelperText/FormHelperText";
+import Button from "@material-ui/core/Button/Button";
+import Box from "@material-ui/core/Box/Box";
+import Container from "@material-ui/core/Container/Container";
+import Grid from "@material-ui/core/Grid/Grid";
+import Typography from "@material-ui/core/Typography/Typography";
 const Registration = () => {
   return (
-    <div>
-      <FormControl>
-        <InputLabel htmlFor="my-input">Email address</InputLabel>
-        <Input id="my-input" />
-      </FormControl>
-    </div>
+    <Container>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <Typography color="primary" variant="h4">
+            Registration Form
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <FormControl fullWidth>
+            <InputLabel htmlFor="my-input">Email address</InputLabel>
+            <Input id="my-input" />
+          </FormControl>
+        </Grid>
+        <Grid item xs={12}>
+          <FormControl fullWidth>
+            <InputLabel htmlFor="my-input">Password</InputLabel>
+            <Input id="my-input" type="password" />
+          </FormControl>
+        </Grid>
+        <Grid item xs={12}>
+          <FormControl fullWidth>
+            <InputLabel htmlFor="my-input">Confirm Password</InputLabel>
+            <Input id="my-input" type="password" />
+          </FormControl>
+        </Grid>
+        <Grid item>
+          <Button color="primary" variant="contained">
+            Register
+          </Button>
+        </Grid>
+      </Grid>
+    </Container>
   );
 };
-
 export default Registration;
