5c9fd1c49c917fdc0aa7e6a9f4b80fae8dfcd230
Jenish Limbu <jlimbu539@gmail.com>
2023-08-06 19:05:43 +0545

    authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,35 @@
 import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import Login from "./pages/Login";
 import Registration from "./pages/Registration";
 import Test from "./pages/Test";
 import "./App.css";
 import Home from "./pages/Home";
+import ProtectedRoute from "./component/ProtectedRoute";
 function App() {
+  const auth = localStorage.getItem("loggedIn");
+
   return (
     <BrowserRouter>
       <Route path="/" exact component={Login} />
       <Route path="/registration" exact component={Registration} />
-      <Route path="/test" exact component={Test}></Route>
-      <Route path="/home" exact component={Home}></Route>
+      <Route path="/test" exact component={Test} />
+      <Route
+        path="/home"
+        exact
+        render={({ location }) =>
+          auth ? (
+            <Home />
+          ) : (
+            <Redirect
+              to={{
+                pathname: "/",
+                state: { from: location },
+              }}
+            />
+          )
+        }
+      />
     </BrowserRouter>
   );
 }
---
+++ src/component/ProtectedRoute.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+
+interface ProtectedRouteProps {
+  children: React.ReactNode;
+}
+
+const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
+  const auth = localStorage.getItem("loggedIn");
+
+  return (
+    <Route
+      render={({ location }) =>
+        auth ? (
+          children
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/login",
+              state: { from: location },
+            }}
+          />
+        )
+      }
+    />
+  );
+};
+
+export default ProtectedRoute;
