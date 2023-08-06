504e6266d5758ef1b37eb9e2b696e431c72ce50e
Jenish Limbu <jlimbu539@gmail.com>
2023-08-06 14:02:29 +0545

    data stored in local storage

--- src/pages/Registration.tsx
+++ src/pages/Registration.tsx
@@ -7,7 +7,26 @@ import { Link } from "react-router-dom";
 import Container from "@material-ui/core/Container/Container";
 import Grid from "@material-ui/core/Grid/Grid";
 import Typography from "@material-ui/core/Typography/Typography";
+import { useState } from "react";
 const Registration = () => {
+  const [contacts, setContacts] = useState<
+    { email: string; password: string; confirmPassword: string }[]
+  >([]);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const addData = () => {
+    const datas = {
+      email: email,
+      password: password,
+      confirmPassword: confirmPassword,
+    };
+    const allContacts = [...contacts, datas];
+    setContacts(allContacts);
+
+    localStorage.setItem("contacts", JSON.stringify(allContacts));
+  };
+
   return (
     <Container>
       <form style={{ width: "650px", margin: "auto" }}>
@@ -19,24 +38,48 @@ const Registration = () => {
           </Grid>
           <Grid item xs={12}>
             <FormControl fullWidth>
-              <InputLabel htmlFor="my-input">Email address</InputLabel>
-              <Input id="my-input" />
+              <InputLabel htmlFor="email">Email address</InputLabel>
+              <Input
+                id="email"
+                value={email}
+                type="email"
+                required
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
+              />
             </FormControl>
           </Grid>
           <Grid item xs={12}>
             <FormControl fullWidth>
-              <InputLabel htmlFor="my-input">Password</InputLabel>
-              <Input id="my-input" type="password" />
+              <InputLabel htmlFor="password">Password</InputLabel>
+              <Input
+                id="password"
+                type="password"
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
+                value={password}
+              />
             </FormControl>
           </Grid>
           <Grid item xs={12}>
             <FormControl fullWidth>
-              <InputLabel htmlFor="my-input">Confirm Password</InputLabel>
-              <Input id="my-input" type="password" />
+              <InputLabel htmlFor="confirmPassword">
+                Confirm Password
+              </InputLabel>
+              <Input
+                id="confirmPassword"
+                type="password"
+                value={confirmPassword}
+                onChange={(e) => {
+                  setConfirmPassword(e.target.value);
+                }}
+              />
             </FormControl>
           </Grid>
           <Grid item style={{ display: "flex", gap: "20px" }}>
-            <Button color="primary" variant="contained">
+            <Button color="primary" variant="contained" onClick={addData}>
               Register
             </Button>
             <div>
@@ -45,6 +88,11 @@ const Registration = () => {
           </Grid>
         </Grid>
       </form>
+      <div>
+        {contacts.map((val, ind) => (
+          <p key={ind}>{val.email}</p>
+        ))}
+      </div>
     </Container>
   );
 };
